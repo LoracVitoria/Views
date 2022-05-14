0fc0c2141c946a3db99b7fab6316662b7453f23f
Fulano de Tal <fulanodetal@exemplo.br>
2022-05-13 22:00:50 -0300

    a3

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -40,6 +42,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnExibir;
     private Button btnLimpar;
+
+    private LinearLayout lblDados;
+    private TextView txtNome;
+    private TextView txtEmail;
+    private TextView txtTelefone;
+    private TextView txtWhats;
+    private TextView txtPeriodo;
+    private TextView txtPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,13 +89,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnLimpar.setOnClickListener(this);
 
         edtNome.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
-//        edtEmail.setFilters(new InputFilter[]{new InputFilter.});
-//        edtEmail.filters = arrayOf(InputFilter {source, _, _, _, _, _ ->source.filter { regex.matches(it.toString()) }
+
+        lblDados = findViewById(R.id.lblDados);
+        txtNome = findViewById(R.id.txtNome);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtTelefone = findViewById(R.id.txtTelefone);
+        txtWhats = findViewById(R.id.txtWhats);
+        txtPeriodo = findViewById(R.id.txtPeriodo);
+        txtPref = findViewById(R.id.txtPref);
     }
     @Override
     public  void onClick(View view){
         if(view.getId() == R.id.btnOk) {
             Toast.makeText(this,"Exibir", Toast.LENGTH_LONG).show();
+            if (view.getId() == R.id.btnOk){
+                //Toast.makeText(this, "Exibir", Toast.LENGTH_LONG).show();
+                lblDados.setVisibility(View.VISIBLE);
+                txtNome.setText("Nome: " + edtNome.getText().toString());
+                txtEmail.setText(edtEmail.getText().toString());
+                txtTelefone.setText(edtTelefone.getText().toString());
+                if (swtWhats.isChecked())
+                    txtWhats.setText("WhatsApp: " + swtWhats.getTextOn());
+                else
+                    txtWhats.setText("WhatsApp: " + swtWhats.getTextOff());
+
+                int idrdbSelecionado = rdgPeriodo.getCheckedRadioButtonId();
+                if (idrdbSelecionado > 0){
+                    RadioButton rdbSelecionado = findViewById(idrdbSelecionado);
+                    txtPeriodo.setText("Período: " + rdbSelecionado.getText().toString());
+                }
+
+                String pref="";
+                if (chbInternet.isChecked())
+                    pref = chbInternet.getText().toString();
+                if (chbTelefone.isChecked()) {
+                    pref += " ";
+                    pref += chbTelefone.getText().toString();
+                }
+                if(chbTv.isChecked()){
+                    pref += " ";
+                    pref += chbTv.getText().toString();
+                }
+                if(chbStreaming.isChecked()){
+                    pref += " ";
+                    pref += chbStreaming.getText().toString();
+                }
+                txtPref.setText("Preferências: " + pref);
+
+            }
+
+
         }else if(view.getId() == R.id.btnCancelar){
             //Toast.makeText(this,"Limpar", Toast.LENGTH_SHORT).show();
             edtNome.setText("");
@@ -102,7 +155,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             chbTv.setChecked(false);
             chbStreaming.setChecked(false);
             chbTelefone.setChecked(false);
-
 
         }
     }
