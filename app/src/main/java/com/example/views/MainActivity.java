d4131fe6f9bd843a432baef4bde44bcd79b2905f
Fulano de Tal <fulanodetal@exemplo.br>
2022-05-20 21:47:34 -0300

    paises

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
+    private Button botao;
+    private TextView txtPais;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botao = findViewById(R.id.btnPais);
+        txtPais = findViewById(R.id.txtPais);
+        botao.setOnClickListener(this);
+
+    }
+    public void onClick(View view){
+        if(view.getId() == R.id.btnPais){
+            txtPais.setVisibility(View.VISIBLE);
+            txtPais.setText(R.string.pais);
+        }
+
     }
 }
